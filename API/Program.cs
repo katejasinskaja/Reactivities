3bd102d9f719c1038e4ef09f15b18cf92e66c4e6
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2024-09-16 18:31:22 +0300

    create a skeleton for client-app

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +32,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 
+app.UseAuthorization();
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
