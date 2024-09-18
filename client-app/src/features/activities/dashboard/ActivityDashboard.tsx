1cc01597e4b3c1658126b8470e60f9f9f4b0e3f1
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2024-09-18 18:13:01 +0300

    Set up axios configuration for communication between UI and backend

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,16 +14,18 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectedActivity,
     selectActivity, cancelSelectActivity,
-editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} selectActivity={selectActivity}
-                    deleteActivity={deleteActivity} />
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}               />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -32,7 +34,9 @@ editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
                         openForm={openForm}
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm closeForm={closeForm}
+                        activity={selectedActivity} createOrEdit={createOrEdit}
+                        submitting={submitting} />}
             </Grid.Column>
         </Grid>
     )
