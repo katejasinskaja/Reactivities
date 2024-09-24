00435c4cffa1859e09109c1121c8091b50139e98
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2024-09-24 21:28:58 +0300

    Add some finishing touches

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -4,35 +4,41 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import NotFound from "../../features/errors/NotFound";
-import ValidationError from "../../features/activities/form/ValidationError";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
+
+            {
+                element: <RequireAuth />, children: [
+                    {
+                        path: 'activities', element: <ActivityDashboard />
+                    },
+                    {
+                        path: 'activities/:id', element: <ActivityDetails />
+                    },
+                    {
+                        path: 'createActivity', element: <ActivityForm key='create' />
+                    },
+                    {
+                        path: 'manage/:id', element: <ActivityForm key='manage' />
+                    },
+                    {
+                        path: 'profiles/:username', element: <ProfilePage />
+                    },
+                    {
+                        path: 'login', element: <LoginForm />
+                    },
+
+            ]},
             
-            {
-                path: 'activities', element: <ActivityDashboard />
-            },
-            {
-                path: 'activities/:id', element: <ActivityDetails />
-            },
-            {
-                path: 'createActivity', element: <ActivityForm key='create' />
-            },
-            {
-                path: 'manage/:id', element: <ActivityForm key='manage'/>
-            },
-            {
-                path: 'profiles/:username', element: <ProfilePage />
-            },
-            {
-                path: 'login', element: <LoginForm />
-            },
+           
             { path: 'not-found', element: <NotFound /> },
             { path: 'server-error', element: <ServerError /> },
             { path: '*', element: <Navigate replace to='/not-found' /> }
